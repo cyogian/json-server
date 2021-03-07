5f35f90c6cdc4cc2c0b90e38efc2c356b56b3f36
Amar Nath Yogi <contact@cyogian.dev>
2021-03-07 18:44:17 +0530

    updated json

--- generate_end_time.py
+++ generate_end_time.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 import random
+import pytz
 
 # db.json generated from Mockaroo Fake Data Generator
 # didn't have an option to add end_time relative to start_time so made this program
@@ -13,21 +14,22 @@ with open("./db_unstructured.json") as f:
 # function to add time delta to start time to get end time
 
 
-def add_delta(input_datetime):
+def add_delta_convert_tz(input_datetime, tz):
     fmt = '%Y-%m-%dT%H:%M:%SZ'
     d = datetime.datetime.strptime(input_datetime, fmt)
     h = random.choice([2, 4, 3, 1])
     t = datetime.timedelta(hours=h)
     r = d + t
-    return r.strftime(fmt)
+    return (d.astimezone(tz).strftime(fmt), r.astimezone(tz).strftime(fmt))
 
 
 for i in range(len(data["members"])):
     member = data["members"][i]
+    tz = pytz.timezone(member['tz'])
     for j in range(len(member["activity_periods"])):
         activity_period = member["activity_periods"][j]
-        activity_period["end_time"] = add_delta(
-            activity_period["start_time"])
+        activity_period["start_time"], activity_period["end_time"] = add_delta_convert_tz(
+            activity_period["start_time"], tz)
         member["activity_periods"][j] = activity_period
 
     data["members"][i] = member
